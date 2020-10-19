d1af1a978617b01dcf443de8b984d74ec6fb1287
SharovAD <72381147+SharovAD@users.noreply.github.com>
2020-10-19 17:49:34 +0500

    Add files via upload

---
+++ MyWindow.h
@@ -0,0 +1,40 @@
+#ifndef EXAMPLEWINDOW_H_
+#define EXAMPLEWINDOW_H_
+
+#include "Window.h"
+
+// Объявление класса ExampleWindow как потомка Window
+class MyWindow: public Window
+{
+public:
+	static constexpr int DEFAULT_WIDTH = 800;
+	static constexpr int DEFAULT_HEIGHT = 600;
+
+protected:
+	double _angle; // Угол поворота модели
+	double _eye_level; // Высота уровня глаз
+
+public:
+
+	// Конструктор класса MyWindow
+	MyWindow(
+			int width = DEFAULT_WIDTH,
+			int height = DEFAULT_HEIGHT);
+
+	// Деструктор класса MyWindow
+	virtual ~MyWindow() = default;
+
+	// Метод начальной настройки окна
+	virtual void setup() override;
+
+	// Метод отрисовки окна
+	virtual void render() override;
+
+	// Метод обработки логики
+	virtual void handle_logic() override;
+};
+
+
+
+
+#endif /* EXAMPLEWINDOW_H_ */
